669618e2d72699c6e011c016a250ab2f0c9dfe1e
zach_ <37415819+recyclable@users.noreply.github.com>
2018-11-06 19:05:27 +0800

    StackMain.java
    stack

---
+++ StackMain.java
@@ -0,0 +1,19 @@
+package stack;
+
+public class StackMain {
+
+	public static void main(String[] args) {
+		Stack s1 = new Stack();
+		s1.push(1);
+		s1.push(2);
+		s1.push(3);
+		s1.push(4);
+
+		s1.printAll();
+		s1.printAllReverse();
+
+		System.out.println("11111");
+
+	}
+
+}
